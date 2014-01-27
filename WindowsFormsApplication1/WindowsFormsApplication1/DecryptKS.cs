e90b04faee625a3f20848393e8b932c6652239b8
Lucas Moran <lucas.moran@snhu.edu>
2014-01-26 19:14:18 -0500

    Fixed Decode implementation within Cipher
    Decode was not properly mapped, so needed to add another map to
    accommodate switching from the keyword to normal alphabet.
    
    Also, added boxes and labels to Decrypt form

--- WindowsFormsApplication1/WindowsFormsApplication1/DecryptKS.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/DecryptKS.cs
@@ -12,9 +12,36 @@ namespace WindowsFormsApplication1
 {
     public partial class DecryptKS : Form
     {
+        private Cipher cipher = new Cipher("");
+
         public DecryptKS()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cipher.Keyword = textBox1.Text;
+            label4.Text = cipher.Keyword.ToUpper();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*char[] array = label5.Text.ToCharArray();
+            
+            //label7.Text = Convert.ToString(input);
+            //string str = "Darth Vader is scary.";
+            
+            string input = textBox2.Text;
+            for (int x = input.Length; x <= x; array[0]++)
+            {
+                label6.Text = textBox2.Text;
+                label7.Text = input.Replace(input.ElementAt(input.Length - input.Length), array[0]);
+            }
+            */
+            string input = textBox2.Text;
+            label6.Text = input;
+            label7.Text = cipher.Decode(input);
         }
     }
 }
